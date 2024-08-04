54782466ee720d9737110837be60221400826363
terablade2001 <terablade2001@yahoo.gr>
2024-08-04 12:16:13 +0300

    (0.02)[I#3]: Trying to change the checkbox in the menu, but this doens't work

--- main.js
+++ main.js
@@ -38,12 +38,55 @@ function setMarkdownTextSize() {
 }
 
 
+function findWrappingSubMenuItem() {
+  var menu
+  for(var i=0; i < app.menu.template.length; i++){
+    menu = app.menu.template[i]
+    if (menu.id == "tools") { break }
+  }
+  var submenu
+  for(var i=0; i < menu.submenu.length; i++){
+    submenu = menu.submenu[i]
+    if (submenu.id == "tools.SetMarkdownTextOptions") { break }
+  }
+  var wrappingSubmenu
+  for(var i=0; i < submenu.submenu.length; i++){
+    wrappingSubmenu = submenu.submenu[i]
+    if (wrappingSubmenu.id == "tool.SetMarkdownTextOptions.updateMarkdownTextWrapping") { break }
+  }
+  return wrappingSubmenu
+}
+
+function setMarkdownTextWrapping(onOff) {
+  wrappingSubmenu = findWrappingSubMenuItem()
+  if (onOff == true) {
+    wrappingSubmenu.updateStates(null,null,{'format.show-shadow': true})
+  } else {
+    wrappingSubmenu.updateStates(null,null,{'format.auto-resize': false})
+  }
+}
+
+function updateMarkdownTextWrapping() {
+  wrapping = prefManager.get("SetMarkdownTextOptions::Wrapping")
+  if ((!wrapping) || (wrapping == false)) {
+    wrapping = true
+  } else {
+    wrapping = false
+  }
+  // prefManager.set("SetMarkdownTextOptions::Wrapping", wrapping)
+  setMarkdownTextWrapping(wrapping)
+}
+
+
 let prefManager
 function init () {
   prefManager = app.preferences
   app.commands.register('SetMarkdownTextOptions:setMarkdownTextSize', setMarkdownTextSize)
+  app.commands.register('SetMarkdownTextOptions:updateMarkdownTextWrapping', updateMarkdownTextWrapping)
   fontSize = prefManager.get("SetMarkdownTextOptions::FontSize")
+  wrapping = prefManager.get("SetMarkdownTextOptions::Wrapping")
   if (fontSize) { setDefinedMarkdownTextSize(fontSize+"px") }
+  if (wrapping) { setMarkdownTextWrapping(wrapping) }
 }
 
 exports.init = init
